57d685086fa9673c30cc3342a1e7a6dc5417ba47
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-02-14 17:00:11 +0200

    Data Seeding

---
+++ EFCore Session3 Demo/Models/DbContextOptionsBuilder.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EFCore_Session3_Demo.Models
+{
+    internal class DbContextOptionsBuilder
+    {
+        internal void UseSqlServer(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- EFCore Session3 Demo/Program.cs
+++ EFCore Session3 Demo/Program.cs
@@ -1,11 +1,34 @@
-﻿namespace EFCore_Session3_Demo
+﻿using EFCore_Session3_Demo.Data;
+using EFCore_Session3_Demo.Models;
+using System;
+
+namespace EFCore_Session3_Demo
 {
     internal class Program
     {
+        private static readonly Models.CompanyDbContext dbContext;
+
         static void Main(string[] args)
         {
-            
-            
+            using CompanyDbContext context = new CompanyDbContext();
+
+          //  CompanyDbContextSeed.Seed(dbContext);
         }
+        internal class CompanyDbContext : DbContext, IDisposable
+        {
+            public void Dispose()
+            {
+                // Dispose resources here
+            }
+        }
+
+        internal class DbContext
+        {
+        }
+
+        
+
     }
 }
+
+
