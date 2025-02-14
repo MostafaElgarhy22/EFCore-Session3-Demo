315e0140700634b2439d8560a550569c7d61edb2
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-02-14 17:23:54 +0200

    1. Explicit Loading

--- EFCore Session3 Demo/Program.cs
+++ EFCore Session3 Demo/Program.cs
@@ -6,43 +6,53 @@ namespace EFCore_Session3_Demo
 {
     internal class Program
     {
-      
+
 
         static void Main(string[] args)
         {
             #region Loading Navigational Properties
 
+            #region 1. Explicit Loading
+
+
             #region Example 01
             //using CompanyDbContext context = new CompanyDbContext();
 
             //var employee = (from E in dbContext.Employees
-            //               where E.Code == 1
-            //               select E).FirstOrDefault();
+            //                where E.Code == 1
+            //                select E).FirstOrDefault();
             //if (employee is not null)
             //{
+            //    DbContext.Entry(employee).Reference(nameof(Employee.Department)).Load(); // Loading Explicitly
+
             //    Console.WriteLine($"Employee: {employee.Name}, Department: {employee.Department?.Name ?? "No Department"}");
-            //} 
-            #endregion
-
-            #region Example 02
-            //var department = (from D in dbContext.Departments
-            //                 where D.DeptId == 10
-            //                 select D).FirstOrDefault();
-
-            //if (department is not null )
-            //{
-            //    Console.WriteLine($"Department: Id = {department.DeptId}, Name = {department.Name}");
-
-            //    foreach(var employee in department.Employees)
-            //    {
-            //        Console.WriteLine($"....Employee: Code = {employee.Code}, Name = {employee.Name}");
-            //    }
-            //} 
-            #endregion
-
-            #endregion
         }
-        internal class CompanyDbContext : DbContext, IDisposable
+        #endregion
+
+        #region Example 02
+        //var department = (from D in dbContext.Departments
+        //                 where D.DeptId == 10
+        //                 select D).FirstOrDefault();
+
+        //if (department is not null )
+        //{
+        //    Console.WriteLine($"Department: Id = {department.DeptId}, Name = {department.Name}");
+
+        //    dbContext.Entry(department).Reference(nameof(Department.Employees)).Load(); // Loading Explicitly
+
+        //    foreach(var employee in department.Employees)
+        //    {
+        //        Console.WriteLine($"....Employee: Code = {employee.Code}, Name = {employee.Name}");
+        //    }
+        //} 
+        #endregion
+        #endregion
+
+
+    
+      #endregion
+    } 
+       internal class CompanyDbContext : DbContext, IDisposable
         {
             public void Dispose()
             {
@@ -57,6 +67,7 @@ namespace EFCore_Session3_Demo
         
 
     }
-}
+
+
 
 
